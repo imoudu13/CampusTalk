8441e449e2e833afbcff5383dfab4d5e48ad4d0d
GavinAshworth <gavinashworth315@gmail.com>
2024-03-21 20:36:50 -0700

    Comments

--- pages/userprofilepage.php
+++ pages/userprofilepage.php
@@ -1,4 +1,6 @@
 <?php require_once('../includes/header.php'); ?>
+// if you want to add custom styles or js link it here
+// custom styles have to override bootstrap styles... can be difficult
     </head>
     <body>
 <?php require_once('../includes/nav.php'); ?>

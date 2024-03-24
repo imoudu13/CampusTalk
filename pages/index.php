8785b992e6bc51c3ff02c5a14a22817ef7a631d8
GavinAshworth <gavinashworth315@gmail.com>
2024-03-24 11:40:03 -0700

    Made admin modal, users display. Still need to add some functionality

--- pages/index.php
+++ pages/index.php
@@ -69,7 +69,24 @@
             </div>
         </div>
     <?php } ?>
-
 </main>
+
+<!-- Modal to display users when admin searches for them -->
+<div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="userModalLabel">Search Results</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="userModalBody">
+                <!-- Users will be populated here -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php require_once('../includes/footer.php'); ?>

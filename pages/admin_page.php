69d249d62aa2626be50285dbf8246b8fd89acd99
GavinAshworth <gavinashworth315@gmail.com>
2024-03-23 20:28:15 -0700

    Added admin page. Not functional yet

---
+++ pages/admin_page.php
@@ -0,0 +1,11 @@
+<?php require_once('../includes/header.php'); ?>
+<link href="../css/admin_page.css" rel="stylesheet">
+<script src="../js/admin_page.js"></script>
+</head>
+<body>
+<?php require_once('../includes/nav.php'); ?>
+<main>
+    <h2>Admin page</h2>
+</main>
+
+<?php require_once('../includes/footer.php'); ?>

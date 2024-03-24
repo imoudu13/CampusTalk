7e88a021f8370d21474e56cf4cdad704eb6412c5
Hadi Razmi <hadiluqman006@gmail.com>
2024-03-24 10:53:36 -0700

    Updated userprofilepage.php

--- pages/userprofilepage.php
+++ pages/userprofilepage.php
@@ -95,33 +95,4 @@
         </div>
 
 </main>
-=======
-<!-- link to style sheet insert here later -->
-<script src="../js/userprofilepage.js"></script> 
-<head>
-
-<body>
-    <?php require_once('../includes/nav.php'); ?>
-    <main>
-        <h1>My Profile</h1>
-        <div>
-            <p><strong>Username: </strong><span id = "username"></span></p>
-            <br>
-            <p><strong>Email: </strong><span id = "email"></span></p>
-            <br>
-            <p><strong>First Name: </strong><span id = "firstName"></span></p>
-            <br>
-            <p><strong>Last Name: </strong><span id = "lastName"></span></p>
-        </div>
-
-        <h2>Change Password</h2>
-        <form id = "passwordForm">
-            <label for="newPassword">New Password: <label>
-                <input type="password" id="newPassword" name = "newPassword">
-                <input type = "submit" value = "Change Password">
-        </form>
-
-
-    </main>
->>>>>>> 74e632925415a7571e0555e674a17f71362e00b8
 </body>

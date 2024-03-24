b6406d5de09d6c752f15d60b17d5ccf81c7447d6
imoudu13 <imouduibrahim11@gmail.com>
2024-03-23 19:01:20 -0700

    just staashing

--- js/index.js
+++ js/index.js
@@ -28,9 +28,14 @@ function displayPostsOnLoad(department){
 
                 link.addEventListener('click', function(event) {
                     if (!event.target.classList.contains('like-btn') && !event.target.classList.contains('comment-input')) {
-                        window.location.href = 'post.php'; // Redirect to post.php
+                        // Store the data-id in session storage
+                        let postId = post.postID; // Assuming post.postID contains the ID
+                        sessionStorage.setItem('postId', postId);
+                        // Redirect to post.php
+                        window.location.href = 'post.php';
                     }
                 });
+                
 
                 let postContainer = document.createElement('div');
                 postContainer.classList.add('post-container');

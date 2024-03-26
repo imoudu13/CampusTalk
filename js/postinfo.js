6ee5048c2a2613d406d08689d39a2109dcf522f4
GavinAshworth <gavinashworth315@gmail.com>
2024-03-26 15:36:00 -0700

    Changes to make admin bar responsive on post.php

--- js/postinfo.js
+++ js/postinfo.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('toggle-columns').addEventListener('click', function() {
+        document.querySelector('.left').classList.toggle('collapsed');
+        document.querySelector('.right').classList.toggle('collapsed');
+    });
     displayDepartmentsOnLoad();
 
     // setInterval(refreshPage, 10000);

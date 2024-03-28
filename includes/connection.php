ea8749ff6ca75e694073b4facf2fcbb3a2a6e0a3
GavinAshworth <111312815+GavinAshworth@users.noreply.github.com>
2024-03-27 21:07:02 -0700

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-function connectToDB(){ return new mysqli("localhost", "77825776", "360@501Rex", "db_77825776"); }
+function connectToDB(){ return new mysqli("localhost", "35669746", "35669746", "db_35669746"); }
 
 // this code is for connecting to the db with xampp
 //function connectToDB(){ return new mysqli("localhost", "root", "", "CampusTalk"); }
